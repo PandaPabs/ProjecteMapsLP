eb59deee2c2e22db1a23cef13b424755268476cc
NIU1744801 <1744801@uab.cat>
2025-11-06 11:44:14 +0100

    Tasca 4ish
    tasca 4

---
+++ VSCE2022 - ALUMNES P1/LP-OSM-DLL-2022/CamiSolucio.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "pch.h"
+#include "CamiBase.h"
+
+class CamiSolucio: public CamiBase
+{
+public:
+	CamiSolucio() {}
+	~CamiSolucio() {}
+
+	std::vector<Coordinate> getCamiCoords() override;
+
+private:
+	
+};
